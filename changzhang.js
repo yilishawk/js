d282ae8d3106c13c55cc3a375652c7ce1b345349
mimang <824383214@qq.com>
2023-10-15 11:47:24 +0000

    更新 'changzhang.js'

--- changzhang.js
+++ changzhang.js
@@ -1,7 +1,7 @@
 var rule={
     title:'厂长资源',
     // host:'https://czspp.com',
-    host:'http://www.czzy03.com',
+    host:'https://cz01.pw/',
     url:'/fyclass/page/fypage',
     // searchUrl:'/xssearch?q=**&f=_all&p=fypage',
     // searchUrl:'/?#s=**;post',
