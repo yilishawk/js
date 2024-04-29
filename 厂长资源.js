ad33219962e995fdd69f3d4c7bf29f597228c5e7
迷芒 <yilishawk@gmail.com>
2024-04-29 06:35:29 +0000

    厂长资源.js 已在 Bitbucket 中在线编辑过

--- 厂长资源.js
+++ 厂长资源.js
@@ -1,3 +1,4 @@
+
 var rule={
 	title:'厂长资源',
 	// host:'https://czspp.com', 
@@ -15,7 +16,7 @@ var rule={
 		]
 	},
 	searchUrl:'https://www.czys.top/daoyongjiekoshibushiyoubing?q=**',
-	searchable:2,
+	searchable:1,
 	filterable:0,
 	headers:{
 		'User-Agent': 'okhttp 4.3.12',
@@ -58,32 +59,17 @@ var rule={
 			input
 		}
 	`,
-	推荐:'.bt_img;ul&&li;*;*;*;*',
-	double:true,
-	一级:'.bt_img&&ul&&li;h3.dytit&&Text;img.lazy&&data-original;.jidi&&Text;a&&href',
-	二级:{
-		"title": "h1&&Text;.moviedteail_list li&&a&&Text",
-		"img": "div.dyimg img&&src",
-		"desc": ".moviedteail_list li:eq(3) a&&Text;.moviedteail_list li:eq(2) a&&Text;.moviedteail_list li:eq(1) a&&Text;.moviedteail_list li:eq(7)&&Text;.moviedteail_list li:eq(5)&&Text",
-		"content": ".yp_context&&Text",
-		"tabs": ".mi_paly_box span",
-		"lists": ".paly_list_btn:eq(#id) a"
-	},
-	搜索:'.search_list&&ul&&li;*;*;*;*',
-	搜索:`js:
-	var html = request(input);
-	var d=[];
-	if (html.indexOf('|')>0){
-		let episodes = html.split('$$$');
-		episodes.forEach(function(ep) {
-			let tp=ep.split('|');
-			d.push({
-			title:tp[1],
-			img:tp[2],
-			desc:tp[3],
-			url:HOST+'/movie/'+tp[0]+'.html'})
-		})
-		setResult(d);
-	}
-	`	
+    推荐:'.bt_img;ul&&li;*;*;*;*',
+    double:true,
+    一级:'.bt_img&&ul&&li;h3.dytit&&Text;img.lazy&&data-original;.jidi&&Text;a&&href',
+    二级:{
+        "title": "h1&&Text;.moviedteail_list li&&a&&Text",
+        "img": "div.dyimg img&&src",
+        "desc": ".moviedteail_list li:eq(3) a&&Text;.moviedteail_list li:eq(2) a&&Text;.moviedteail_list li:eq(1) a&&Text;.moviedteail_list li:eq(7)&&Text;.moviedteail_list li:eq(5)&&Text",
+        "content": ".yp_context&&Text",
+        "tabs": ".mi_paly_box span",
+        "lists": ".paly_list_btn:eq(#id) a"
+    },
+    搜索:'.search_list&&ul&&li;*;*;*;*',
+    // 预处理:'rule_fetch_params.headers.Cookie="68148872828e9f4d64e7a296f6c6b6d7=5429da9a54375db451f7f9e4f16ce0ea;esc_search_captcha=1";let new_host="https://czspp.com";let new_html=request(new_host);if(/正在进行人机识别/.test(new_html)){let new_src=pd(new_html,"script&&src",new_host);log(new_src);let hhtml=request(new_src,{withHeaders:true});let json=JSON.parse(hhtml);let html=json.body;let key=html.match(new RegExp(\'var key="(.*?)"\'))[1];let avalue=html.match(new RegExp(\'value="(.*?)"\'))[1];let c="";for(let i=0;i<avalue.length;i++){let a=avalue[i];let b=a.charCodeAt();c+=b}let value=md5(c);log(value);let yz_url="https://czspp.com/a20be899_96a6_40b2_88ba_32f1f75f1552_yanzheng_ip.php?type=96c4e20a0e951f471d32dae103e83881&key="+key+"&value="+value;log(yz_url);hhtml=request(yz_url,{withHeaders:true});json=JSON.parse(hhtml);let setCk=Object.keys(json).find(it=>it.toLowerCase()==="set-cookie");let cookie=setCk?json[setCk].split(";")[0]:"";log("cookie:"+cookie);rule_fetch_params.headers.Cookie=cookie;setItem(RULE_CK,cookie)}',
 }
