de356b5a520b8665a37801948785b0389ce60d77
迷芒 <yilishawk@gmail.com>
2024-11-03 03:45:07 +0000

    厂长资源.js 已在 Bitbucket 中在线编辑过

--- 厂长资源.js
+++ 厂长资源.js
@@ -27,37 +27,24 @@ var rule={
 
 	// lazy代码:源于海阔香雅情大佬 / 小程序：香情影视 https://pastebin.com/L4tHdvFn
 	lazy:`js:
-		pdfh = jsp.pdfh;
-		var html = request(input);
-		var ohtml = pdfh(html, '.videoplay&&Html');
-		var url = pdfh(ohtml, "body&&iframe&&src");
-		if (/Cloud/.test(url)) {
-			var ifrwy = request(url);
-			let code = ifrwy.match(/var url = '(.*?)'/)[1].split('').reverse().join('');
-			let temp = '';
-			for (let i = 0x0; i < code.length; i = i + 0x2) {
-				temp += String.fromCharCode(parseInt(code[i] + code[i + 0x1], 0x10))
-			}
-			input = {
-				jx: 0,
-				url: temp.substring(0x0, (temp.length - 0x7) / 0x2) + temp.substring((temp.length - 0x7) / 0x2 + 0x7),
-				parse: 0
-			}
-		} else if (/decrypted/.test(ohtml)) {
-			var phtml = pdfh(ohtml, "body&&script:not([src])&&Html");
-			eval(getCryptoJS());
-			var scrpt = phtml.match(/var.*?\\)\\);/g)[0];
-			var data = [];
-			eval(scrpt.replace(/md5/g, 'CryptoJS').replace('eval', 'data = '));
-			input = {
-				jx: 0,
-				url: data.match(/url:.*?[\\'\\"](.*?)[\\'\\"]/)[1],
-				parse: 0
-			}
-		} else {
-			input
-		}
-	`,
+  let html = request(input);
+  let hconf = html.match(/r player_.*?=(.*?)</)[1];
+  let json = JSON5.parse(hconf);
+  let url = json.url;
+  if (json.encrypt == '1') {
+    url = unescape(url);
+  } else if (json.encrypt == '2') {
+    url = unescape(base64Decode(url));
+  }
+  if (/\\.(m3u8|mp4|m4a|mp3)/.test(url)) {
+    input = {
+      parse: 0,
+      jx: 0,
+      url: url,
+    };
+  } else {
+    input;
+  }`,
     推荐:'.bt_img;ul&&li;*;*;*;*',
     double:true,
     一级:'.bt_img&&ul&&li;h3.dytit&&Text;img.lazy&&data-original;.jidi&&Text;a&&href',
@@ -86,6 +73,3 @@ var rule={
 	}
 	`	
 }
-   // 搜索:'*',
-    // 预处理:'rule_fetch_params.headers.Cookie="68148872828e9f4d64e7a296f6c6b6d7=5429da9a54375db451f7f9e4f16ce0ea;esc_search_captcha=1";let new_host="https://czspp.com";let new_html=request(new_host);if(/正在进行人机识别/.test(new_html)){let new_src=pd(new_html,"script&&src",new_host);log(new_src);let hhtml=request(new_src,{withHeaders:true});let json=JSON.parse(hhtml);let html=json.body;let key=html.match(new RegExp(\'var key="(.*?)"\'))[1];let avalue=html.match(new RegExp(\'value="(.*?)"\'))[1];let c="";for(let i=0;i<avalue.length;i++){let a=avalue[i];let b=a.charCodeAt();c+=b}let value=md5(c);log(value);let yz_url="https://czspp.com/a20be899_96a6_40b2_88ba_32f1f75f1552_yanzheng_ip.php?type=96c4e20a0e951f471d32dae103e83881&key="+key+"&value="+value;log(yz_url);hhtml=request(yz_url,{withHeaders:true});json=JSON.parse(hhtml);let setCk=Object.keys(json).find(it=>it.toLowerCase()==="set-cookie");let cookie=setCk?json[setCk].split(";")[0]:"";log("cookie:"+cookie);rule_fetch_params.headers.Cookie=cookie;setItem(RULE_CK,cookie)}',
-
