4d9004d95acfeddc6784c1c6a871538644718103
迷芒 <yilishawk@gmail.com>
2025-01-14 04:07:06 +0000

    农民影视.js 已在 Bitbucket 中在线编辑过

--- drpy/农民影视.js
+++ drpy/农民影视.js
@@ -33,8 +33,16 @@ var rule={
     class_name:'国产剧&电视剧&电影&综艺&动漫&短剧',//静态分类名称拼接
     class_url:'12&2&1&3&4&26',//静态分类标识拼接
   play_parse: true,
-  lazy: "js:\n  let html = request(input);\n  let hconf = html.match(/r player_.*?=(.*?)</)[1];\n  let json = JSON5.parse(hconf);\n  let url = json.url;\n  if (json.encrypt == '1') {\n    url = unescape(url);\n  } else if (json.encrypt == '2') {\n    url = unescape(base64Decode(url));\n  }\n  if (/\\.(m3u8|mp4|m4a|mp3)/.test(url)) {\n    input = {\n      parse: 0,\n      jx: 0,\n      url: url,\n    };\n  } else {\n    input = url && url.startsWith('http') && tellIsJx(url) ? {parse:0,jx:1,url:url}:input;\n  }",
-	limit:6,
+     lazy: $js.toString(() => {
+        let init_js = `Object.defineProperties(navigator, {platform: {get: () => 'iPhone'}});`;
+        input = {
+            parse: 1,
+            url: input,
+            js: `try{location.href = document.querySelectorAll("iframe")[1].src;}catch(err) {}document.querySelector(".line").click()`,
+            parse_extra: '&init_script=' + encodeURIComponent(base64Encode(init_js)),
+        }
+    }),
+limit:6,
 	推荐:'.globalPicList .resize_list;*;img&&data-src;*;*',
 	一级:'.globalPicList li;.sTit&&Text;img&&src;.sBottom--em&&Text;a&&href',
 	二级:{
